9df785624bbc8249f888b124315b0da8d16adb1b
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2018-11-12 16:09:33 +0100

    REMOVE frdm-kw41z disable LED init

--- boards/frdm-kw41z/board.c
+++ boards/frdm-kw41z/board.c
@@ -67,8 +67,8 @@ void board_init(void)
     gpio_set(LED0_PIN);
     gpio_init(LED1_PIN, GPIO_OUT);
     gpio_set(LED1_PIN);
-    gpio_init(LED2_PIN, GPIO_OUT);
-    gpio_set(LED2_PIN);
-    gpio_init(LED3_PIN, GPIO_OUT);
-    gpio_set(LED3_PIN);
+    //~ gpio_init(LED2_PIN, GPIO_OUT);
+    //~ gpio_set(LED2_PIN);
+    //~ gpio_init(LED3_PIN, GPIO_OUT);
+    //~ gpio_set(LED3_PIN);
 }
